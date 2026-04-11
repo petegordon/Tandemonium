9a1625144448cc39979c2eb8ac6b3586427db2f7
petegordon <petegordon@users.noreply.github.com>
2026-04-11 18:30:59 +0000

    deploy: cc97d208a5696bc738933a516f00a1869bdfd177

--- pr-preview/pr-194/worker/migrations/0003_rename_normal_to_adventurous.sql
+++
@@ -1,4 +0,0 @@
--- Migration: 0003_rename_normal_to_adventurous.sql
--- Rename difficulty 'normal' to 'adventurous' in existing records
-
-UPDATE scores SET difficulty = 'adventurous' WHERE difficulty = 'normal';
