e2410db62d9b4e971400f5631258c20e78633c80
petegordon <petegordon@users.noreply.github.com>
2026-09-07 20:18:48 +0000

    Remove preview for PR 371 🛬

--- pr-preview/pr-371/worker/migrations/0006_webrtc_quality.sql
+++
@@ -1,4 +0,0 @@
--- Migration 0006: Add WebRTC quality metrics to rooms
-ALTER TABLE rooms ADD COLUMN avg_rtt_ms INTEGER;
-ALTER TABLE rooms ADD COLUMN max_rtt_ms INTEGER;
-ALTER TABLE rooms ADD COLUMN packet_loss_pct REAL;
